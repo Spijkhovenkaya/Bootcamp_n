550ab9fe3ff820418745f1e807a6454e21ee5f66
Spijkhovenkaya <143703649+Spijkhovenkaya@users.noreply.github.com>
2023-09-13 10:19:19 +0200

    Add files via upload

---
+++ Deel_2/Deel_2/Les_12/uitwerkingen/opdracht 1.py
@@ -0,0 +1,7 @@
+naam = input("Wat is jouw naam?")
+leeftijd = input('Hallo, ' + naam + ", wat is jouw leeftijd?")
+
+leeftijd = int(leeftijd)
+nieuwleeftijd = leeftijd + 5
+
+print("Over 5 jaar ben je " + str(nieuwleeftijd) + " jaar oud.")
---
+++ Deel_2/Deel_2/Les_12/uitwerkingen/opdracht 2.py
@@ -0,0 +1,6 @@
+getal1 = float(input("Voer het eerste getal in: "))
+getal2 = float(input("Voer het tweede getal in: "))
+
+gemiddelde = (getal1 + getal2) / 2
+
+print(f"Het gemiddelde van {getal1} en {getal2} is: {gemiddelde}")
